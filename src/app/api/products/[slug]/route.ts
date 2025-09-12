0131de5e6ea779f3e12dda44170cf149d69adc36
fellipesnoww <fellipe2.0e@gmail.com>
2025-09-11 22:14:11 -0300

    chore: remove console log

--- src/app/api/products/[slug]/route.ts
+++ src/app/api/products/[slug]/route.ts
@@ -9,10 +9,8 @@ export async function GET(
   await new Promise((resolve) => setTimeout(resolve, 1000))
 
   const slug = z.string().parse(params.slug)
-  console.log("slug", slug);
 
   const product = data.products.find((product) => product.slug === slug)
-  console.log("product", product);
 
   if (!product) {
     return Response.json({ message: 'Product not found.' }, { status: 400 })
